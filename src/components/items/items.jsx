ef42caa9f8ab5ff108d726a2f2a08a5bef4768d7
a5h101 <ak45hhere@gmail.com>
2023-02-24 21:50:00 +0530

    day-4 creaking fake data and displaying the cards

--- src/components/items/items.jsx
+++ src/components/items/items.jsx
@@ -1,11 +1,46 @@
 import './items.css';
-import itemImg from '../../images/itemImg.svg';
+// import itemImg from '../../images/itemImg.svg';
 import { Link } from 'react-router-dom';
+import fakedata from '../../fakedata.json';
 const Items = () => {
   return (
     <>
       <div className="allItems">
-        <Link to="itemDetail">
+        {fakedata &&
+          fakedata.map(data => {
+            return (
+              <Link to="itemDetail">
+                <div className="item" key={data.id}>
+                  <div id="itemImgBox">
+                    <img className="imgItem" src={data.image} alt="cover" />
+                  </div>
+                  <div className="line lineItems"></div>
+                  <div className="itemInfo">
+                    <div className="itemName">
+                      <span>{data.product}</span>
+                    </div>
+                    <div className="itemDetail">{data.description}</div>
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
+        {/* <div className="item">
+            <div id="itemImgBox">
+              <img className="imgItem" src={itemImg} alt="cover" />
+            </div>
+            <div className="line"></div>
+            <div className="itemInfo">
+              <div className="itemName">
+                <span>Iron 1000W</span>
+              </div>
+              <div className="itemDetail">
+                Description of my product will be written in this area. This
+                will be the maximum space that can be taken by a card.
+              </div>
+            </div>
+          </div> */}
+        {/* <Link to="itemDetail">
           <div className="item">
             <div id="itemImgBox">
               <img className="imgItem" src={itemImg} alt="cover" />
@@ -191,24 +226,7 @@ const Items = () => {
               </div>
             </div>
           </div>
-        </Link>
-        <Link to="itemDetail">
-          <div className="item">
-            <div id="itemImgBox">
-              <img className="imgItem" src={itemImg} alt="cover" />
-            </div>
-            <div className="line"></div>
-            <div className="itemInfo">
-              <div className="itemName">
-                <span>Iron 1000W</span>
-              </div>
-              <div className="itemDetail">
-                Description of my product will be written in this area. This
-                will be the maximum space that can be taken by a card.
-              </div>
-            </div>
-          </div>
-        </Link>
+        </Link> */}
       </div>
     </>
   );
