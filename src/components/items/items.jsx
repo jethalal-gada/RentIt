614a333024522713d5019bcc7faa3da2a7bd9816
a5h101 <ak45hhere@gmail.com>
2023-02-27 14:58:56 +0530

    day5

--- src/components/items/items.jsx
+++ src/components/items/items.jsx
@@ -1,28 +1,38 @@
 import './items.css';
 // import itemImg from '../../images/itemImg.svg';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import fakedata from '../../fakedata.json';
 const Items = () => {
+  const navigate = useNavigate();
+
+  const handleClick = data => {
+    navigate('itemDetail', { state: data });
+  };
+
   return (
     <>
       <div className="allItems">
         {fakedata &&
           fakedata.map(data => {
             return (
-              <Link to="itemDetail">
-                <div className="item" key={data.id}>
-                  <div id="itemImgBox">
-                    <img className="imgItem" src={data.image} alt="cover" />
-                  </div>
-                  <div className="line lineItems"></div>
-                  <div className="itemInfo">
-                    <div className="itemName">
-                      <span>{data.product}</span>
-                    </div>
-                    <div className="itemDetail">{data.description}</div>
-                  </div>
+              // <Link to="itemDetail">
+              <div
+                className="item"
+                onClick={() => handleClick(data)}
+                key={data.id}
+              >
+                <div id="itemImgBox">
+                  <img className="imgItem" src={data.image} alt="cover" />
                 </div>
-              </Link>
+                <div className="line lineItems"></div>
+                <div className="itemInfo">
+                  <div className="itemName">
+                    <span>{data.product}</span>
+                  </div>
+                  <div className="itemDetail">{data.description}</div>
+                </div>
+              </div>
+              // </Link>
             );
           })}
       </div>
