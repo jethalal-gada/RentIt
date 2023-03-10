733806b1fc1a0b6dd47d397bc40c2b3a2dcf39ea
a5h101 <ak45hhere@gmail.com>
2023-03-10 18:09:51 +0530

    made api in backend and fetched in front-end

--- Rentit-backend/app.js
+++ Rentit-backend/app.js
@@ -2,11 +2,46 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 2000;
+const cors = require('cors');
+app.use(cors());
+const items = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/fakedata.json`)
+);
+
+//Middlewares
 app.use(express.json());
+
+const getItems = (req, res) => {
+  // console.log(req.requestTime);
+  res.status(200).json({
+    status: 'sucess',
+    results: items.length,
+    // reqAt: req.requestTime,
+    data: {
+      items: items,
+    },
+  });
+};
+const getItemDetails = (req, res) => {
+  const item = items.find((e) => e.id == req.params.id);
+
+  if (req.params.id > items.length - 1)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'imvalid ID',
+    });
+
+  res.status(200).json({
+    status: 'sucess',
+    data: {
+      item: item,
+    },
+  });
+};
+
+app.route('/api-rentit/v1/items').get(getItems);
+app.route('/api-rentit/v1/items/:id').get(getItemDetails);
 
 app.listen(port, () => {
   console.log(`listening port ${port}...`);
-});
-app.get('/', (req, res) => {
-  res.end('hi');
 });
--- Rentit-frontend/src/components/items/items.jsx
+++ Rentit-frontend/src/components/items/items.jsx
@@ -1,7 +1,16 @@
 import './items.css';
 import { useNavigate } from 'react-router-dom';
 import fakedata from '../../fakedata.json';
+import { useEffect, useState } from 'react';
 const Items = () => {
+  const [itemData, setItemData] = useState(null);
+
+  // useEffect(() => {
+  fetch('http://127.0.0.1:2000/api-rentit/v1/items')
+    .then((response) => response.json())
+    .then((data) => setItemData(data));
+  // console.log(itemData);
+  // });
   const navigate = useNavigate();
 
   const handleClick = (data) => {
