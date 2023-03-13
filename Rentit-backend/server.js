21d4e2d84d0fd8931bcf3381770d6c7a6333f3da
a5h101 <ak45hhere@gmail.com>
2023-03-13 18:15:12 +0530

    mongoose constructor changed

--- Rentit-backend/server.js
+++ Rentit-backend/server.js
@@ -14,7 +14,7 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    // useUnifiedTopology: true,
+    useUnifiedTopology: true, //optional
   })
   .then((con) => console.log('DB connction stablished'))
   .catch((err) => console.log(err));
