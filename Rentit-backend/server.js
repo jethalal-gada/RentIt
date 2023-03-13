9d01cad2cedca6716d748c92f2e36898fb598632
a5h101 <ak45hhere@gmail.com>
2023-03-13 18:13:08 +0530

    nodeJS connected to database

--- Rentit-backend/server.js
+++ Rentit-backend/server.js
@@ -1,6 +1,24 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = require('./app');
 
-const port = 2000;
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    // useUnifiedTopology: true,
+  })
+  .then((con) => console.log('DB connction stablished'))
+  .catch((err) => console.log(err));
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`listening port ${port}...`);
 });
