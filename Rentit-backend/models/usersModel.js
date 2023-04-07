5fe9244c7562725739d6c487d0b4a4feb7685719
a5h101 <ak45hhere@gmail.com>
2023-04-07 11:24:09 +0530

    added validator in user schema

--- Rentit-backend/models/usersModel.js
+++ Rentit-backend/models/usersModel.js
@@ -19,11 +19,23 @@ const userSchema = new mongoose.Schema({
   picture: { type: String, required: true },
   sub: String,
   token_type: { type: String, required: true },
-  savedProducts: [
-    { type: mongoose.Schema.Types.ObjectId, ref: 'product' },
-    { unique: true },
-    { required: false },
-  ],
+  // savedProducts: [
+  //   { type: mongoose.Schema.Types.ObjectId, ref: 'product' },
+  //   { unique: true },
+  //   { required: false },
+  //   { maxLength: 5 },
+  // ],
+  savedProducts: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'product' }],
+    unique: true,
+    required: false,
+    validate: {
+      validator: function (v) {
+        return v.length <= 5;
+      },
+      message: 'The maximum allowed number of saved products is 5.',
+    },
+  },
   postedProducts: [{ type: String }, { required: false }],
 });
 
