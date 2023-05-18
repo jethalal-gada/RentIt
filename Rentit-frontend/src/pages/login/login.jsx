4d7513fae87f81a4e20628f516b4b5282be97da5
a5h101 <ak45hhere@gmail.com>
2023-05-18 11:26:12 +0530

    naming

--- Rentit-frontend/src/pages/login/login.jsx
+++ Rentit-frontend/src/pages/login/login.jsx
@@ -4,7 +4,7 @@ import { FcGoogle } from 'react-icons/fc';
 import { GoMarkGithub } from 'react-icons/go';
 import { SiFacebook } from 'react-icons/si';
 import { Link } from 'react-router-dom';
-import Animation from '../../components/steamCss/animation';
+import Animation from '../../components/SteamCss/animation';
 import { LoginSocialGoogle } from 'reactjs-social-login';
 import { useNavigate } from 'react-router-dom';
 import UserDetails from '../userDetails/userDetails';
@@ -22,25 +22,25 @@ const Login = () => {
   //If the user is not logged in then return the Login page
   return (
     <>
-      <div className='loginBox'>
-        <div className='signInTop signIn'>
-          <div className='logoBox'>
-            <Link to='/'>
-              <img className='signInLogo ' src={logo} alt='logo' />
+      <div className="loginBox">
+        <div className="signInTop signIn">
+          <div className="logoBox">
+            <Link to="/">
+              <img className="signInLogo " src={logo} alt="logo" />
             </Link>
           </div>
-          <div className='line lineLogin'></div>
+          <div className="line lineLogin"></div>
         </div>
-        <div className='wlcmTxt'>
+        <div className="wlcmTxt">
           <h3> Choose any Login method</h3>
         </div>
         <Animation />
-        <div className='signIn'>
+        <div className="signIn">
           <LoginSocialGoogle //Google login uising LoginSocialGoogle(npm package)
             client_id={`${process.env.REACT_APP_CLIENT_ID}`}
-            scope='openid profile email'
-            discoveryDocs='claims_supported'
-            access_type='offline'
+            scope="openid profile email"
+            discoveryDocs="claims_supported"
+            access_type="offline"
             onResolve={({ provider, data }) => {
               setLoginObj(data);
               sessionStorage.setItem(
@@ -57,27 +57,27 @@ const Login = () => {
               );
               navigate('/'); //navigate to home page after sucessfull login
             }}
-            onReject={(err) => {
+            onReject={err => {
               alert('Failed to login');
               console.log(err, 'Failed');
             }}
           >
-            <button className='loginBtn'>
-              <FcGoogle className='g signInIcons' size={25} />
+            <button className="loginBtn">
+              <FcGoogle className="g signInIcons" size={25} />
               Login with Google
             </button>
           </LoginSocialGoogle>
 
-          <button className='loginBtn'>
-            <GoMarkGithub className='gh signInIcons' size={25} />
+          <button className="loginBtn">
+            <GoMarkGithub className="gh signInIcons" size={25} />
             Login with GitHub
           </button>
-          <button className='loginBtn'>
-            <SiFacebook className='fb signInIcons' size={25} />
+          <button className="loginBtn">
+            <SiFacebook className="fb signInIcons" size={25} />
             Login with Facebook
           </button>
         </div>
-        <div className='signInTxt signIn'>
+        <div className="signInTxt signIn">
           All you personl detials are safe with us.
         </div>
       </div>
